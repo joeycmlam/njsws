2ba60c96bb6727aaded786643282b8f45b7e0bf7
joeylam <joey.cm.lam@gmail.com>
2021-05-02 17:47:09 +0800

    revised for path sample

--- src/v1.js
+++ src/v1.js
@@ -1,4 +1,12 @@
 
-const msg = 'my first nodejs in visual studio!'
+const msg = 'my first nodejs in visual studio!';
 
-console.log(msg)
+const path = require('path');
+
+console.log(`the file name is ${path.basename(__filename)}`);
+
+console.log(msg);
+
+console.log(__dirname);
+
+console.log(__filename);
